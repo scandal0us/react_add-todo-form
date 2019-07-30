60e0959635c7f2a2a583a75ea8d629f43c358abb
scandal0us <d.l.lutsak@gmail.com>
2019-07-30 20:14:19 +0300

    Updates

---
+++ precache-manifest.aeb4022086e0a40a60b3569cc730d492.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "481301b9f3357a46ce66f2421274f18e",
+    "url": "/react_add-todo-form/index.html"
+  },
+  {
+    "revision": "8b106265ce7137b67d27",
+    "url": "/react_add-todo-form/static/css/main.7beac8a9.chunk.css"
+  },
+  {
+    "revision": "bd0c9146313fb134b61e",
+    "url": "/react_add-todo-form/static/js/2.7c0a1338.chunk.js"
+  },
+  {
+    "revision": "8b106265ce7137b67d27",
+    "url": "/react_add-todo-form/static/js/main.0b2f6ec0.chunk.js"
+  },
+  {
+    "revision": "4953b3d381bec0cda918",
+    "url": "/react_add-todo-form/static/js/runtime~main.9d188fa2.js"
+  }
+]);
